2e5f604de6f3f715006044a8348c03eb588590e5
PeterC <petercbg@abv.bg>
2023-02-26 20:33:35 +0200

    sssssssssssss

--- db.config.js
+++ db.config.js
@@ -1,8 +1,6 @@
 module.exports = {
-    HOST: "eu-cdbr-west-03.cleardb.net",
-    USER: "be2474d9c0d341",
-    PASSWORD: "d3a9406a",
-    DB: "heroku_400f28e9054929d"
-
-   
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "12345678",
+    DB: "nodeapp"
   };
