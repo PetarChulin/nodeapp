1f7c4014408cf83de7723f01a7399bdd40304226
PeterC <petercbg@abv.bg>
2023-02-26 17:24:28 +0200

    sssssssssss

--- db.config.js
+++ db.config.js
@@ -1,4 +1,8 @@
-export const HOST = "eu-cdbr-west-03.cleardb.net";
-export const USER = "be2474d9c0d341";
-export const PASSWORD = "d3a9406a";
-export const DB = "heroku_400f28e9054929d";
+module.exports = {
+    HOST: "eu-cdbr-west-03.cleardb.net",
+    USER: "be2474d9c0d341",
+    PASSWORD: "d3a9406a",
+    DB: "heroku_400f28e9054929d"
+
+   
+  };
